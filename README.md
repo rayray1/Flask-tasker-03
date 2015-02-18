d96d69aed7da4e8758ee18485ea8cb8430a2288d
rayray1 <rupiray@yahoo.com>
2015-02-18 02:39:15 +0000

    flask task manager

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Flask Task Manager Part 3 - Error Handling and Unit Testing
+Flask Task Manager Part 3 :
+
+Error Handling and Unit Testing
+Styles, Test Coverage and Permissions
+Blueprints
--- app/views.py
+++ app/views.py
@@ -35,6 +35,7 @@ def login_required(test):
 def logout():
     session.pop('logged_in', None)
     session.pop('user_id', None)
+    session.pop('role', None)
     flash('You are logged out. Bye. :(')
     return redirect(url_for('login'))
 
@@ -60,6 +61,7 @@ def login():
             else:
                 session['logged_in'] = True
                 session['user_id'] = u.id
+                session['role'] = u.role
                 flash('You are logged in. Go Crazy.')
                 return redirect(url_for('tasks'))
         else:
@@ -113,8 +115,8 @@ def new_task():
             return redirect(url_for('tasks'))
         else:
             return render_template('tasks.html', form=form, error=error)
-    if request.method == 'GET':
-        return render_template('tasks.html', form=form)
+    # if request.method == 'GET':
+    #     return render_template('tasks.html', form=form)
 
 
 # Mark tasks as complete:
@@ -122,10 +124,15 @@ def new_task():
 @login_required
 def complete(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).update({"status":"0"})
-    db.session.commit()
-    flash('The task was marked as complete.')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id:
+        task.update({"status":"0"})
+        db.session.commit()
+        flash('The task was marked as complete.')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only update tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 
@@ -134,10 +141,15 @@ def complete(task_id):
 @login_required
 def delete_entry(task_id):
     new_id = task_id
-    db.session.query(Task).filter_by(task_id=new_id).delete()
-    db.session.commit()
-    flash('The task was deleted. Want to add a new one?')
-    return redirect(url_for('tasks'))
+    task = db.session.query(Task).filter_by(task_id=new_id)
+    if session['user_id'] == task.first().user_id:
+        task.delete()
+        db.session.commit()
+        flash('The task was deleted. Want to add a new one?')
+        return redirect(url_for('tasks'))
+    else:
+        flash('You can only delete tasks that belong to you.')
+        return redirect(url_for('tasks'))
 
 
 # User Registration
